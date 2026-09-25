3e9319a039cf715acceba9ca89abb824625f5501
Murugan Kasi <murugankasi2004@gmail.com>
2026-09-25 23:15:55 +0530

    Include Tesseract WASM dependency in production

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  serverExternalPackages: ["tesseract.js", "bmp-js"],
+  serverExternalPackages: [
+    "tesseract.js",
+    "bmp-js",
+    "wasm-feature-detect",
+  ],
 
   outputFileTracingIncludes: {
     "/*": [
       "./node_modules/tesseract.js/**/*",
       "./node_modules/tesseract.js-core/**/*",
       "./node_modules/bmp-js/**/*",
+      "./node_modules/wasm-feature-detect/**/*",
     ],
   },
 };
